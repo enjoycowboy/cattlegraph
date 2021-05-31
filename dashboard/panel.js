ef58565fcd5eca660136f5896220b9fbef1f414a
enjoycowboy <eveiga@msn.com>
2021-05-31 02:11:33 -0300

    debug 1

---
+++ dashboard/panel.js
@@ -0,0 +1,13 @@
+const loteInput = document.getElementById('lote_input');
+const prazoInput = document.getElementById('prazo_input');
+const valorInput = document.getElementById('preco_input');
+
+function update(){
+	const data = {Prazo:prazoInput.value, Valor:valorInput.value};
+	nodecg.sendMessage('atualiza', data);
+}
+
+function buscaLote(){
+	const data = {input:loteInput.value};
+	nodecg.sendMessage('load', data);
+}
